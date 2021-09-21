ca1837da21d1864290d64ca3d662267118f9ffc7
SzymonJag15 <69639056+SzymonJag15@users.noreply.github.com>
2021-09-21 18:19:29 +0200

    Add event to button (#2)
    Co-authored-by: szymon <szymon.jagielski@olicom.pl>

---
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'components/atoms/Button/Button';
+import { StyledAverage, StyledInfo, Wrapper } from './UsersListItem.styles';
+
+const UsersListItem = ({ index, userData: { average, name, attendance = '0%' } }) => {
+  const showindex = (index) => console.log(index);
+
+  return (
+    <Wrapper>
+      <StyledAverage value={average}>{average}</StyledAverage>
+      <StyledInfo>
+        <p>
+          {name}
+          <Button onClick={() => showindex(index)} />
+        </p>
+        <p>attendance: {attendance}</p>
+      </StyledInfo>
+    </Wrapper>
+  );
+};
+
+UsersListItem.propTypes = {
+  userData: PropTypes.shape({
+    average: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    attendance: PropTypes.string,
+  }),
+};
+
+export default UsersListItem;
